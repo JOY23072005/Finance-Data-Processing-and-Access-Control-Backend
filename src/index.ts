679d302e6408b4e37b181de06e1d007ebf79b395
Joydeep Hans <hansjoydeep@gmail.com>
2026-04-05 17:27:52 +0530

    refactor: / endpoint res added 'backend is running'

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,9 @@ const PORT = process.env.PORT || 5001;
 
 app.use(cors());
 app.use(express.json());
-
+app.get("/",(req,res)=>{
+    res.send("Backend is Up and Running!");
+})
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/records", recordRoutes);
