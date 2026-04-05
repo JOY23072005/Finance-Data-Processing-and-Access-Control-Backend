2e200ccaa6c3c60fd3b0b21f8d17257074a83c0f
Joydeep Hans <hansjoydeep@gmail.com>
2026-04-05 17:46:22 +0530

    feat:added serverless-http for vercel deployment

--- src/index.ts
+++ src/index.ts
@@ -16,9 +16,9 @@ const PORT = process.env.PORT || 5001;
 app.use(cors());
 app.use(express.json());
 
-app.get("/",(req,res)=>{
-    res.send("Backend is Up and Running!");
-})
+app.get("/", (req, res) => {
+  res.json({ message: "API is running" });
+});
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
